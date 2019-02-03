e831ae34e96bbfa5e1a9da2b94b6eb24354bffe4
Henry <henrycarmenateg@gmail.com>
2019-02-03 15:48:14 -0500

    Remove Conflict

--- posts/models.py
+++ posts/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 
 class Posts(models.Model):
-
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class ImagePosts(models.Model):
     pass
 
 # Create your models here.
