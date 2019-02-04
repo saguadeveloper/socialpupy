456c789223fd9a2036bf1e10bfbaaf6bc310fa66
Henry <henry.garcia@htlkeys.com>
2019-02-04 14:04:29 -0500

    Add structure for dashboard

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class DashboardView(TemplateView):
+    template_name = "dashboard/index.html"
+
 
 # Create your views here.
